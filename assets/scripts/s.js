2bc1265c35ff29acc4a7762f67027ccad8f60bca
t.guz4 <dbxwxqp@gmail.com>
2025-08-04 15:22:03 +0300

    Marquee

--- assets/scripts/s.js
+++ assets/scripts/s.js
@@ -11,3 +11,42 @@
     })
     .catch((error) => console.log('error', error))
 */
+
+/* < marquee */
+const marqueeLists= document.querySelectorAll('.marquee__list')
+
+const animationTime = 10 * 1000 // css animation time
+const gap = 12 // css gap
+
+const cloneChildrens = (removeOff = true) => {
+    const headerItems = marqueeLists[0].querySelectorAll('.marquee__item')
+    for(let i = 0; i < 3; i++) {
+        Array.from(marqueeLists).forEach((marqueeList) => {
+            const cloneItem = headerItems[i].cloneNode(true)
+            marqueeList.append(cloneItem)
+
+            const marqueeListItems = marqueeList.querySelectorAll('.marquee__item')
+            if(removeOff) marqueeListItems[0].parentNode.removeChild(marqueeListItems[0])
+        })
+    }
+}
+for(let i = 0; i < 9; i++){
+    cloneChildrens(false)
+}
+setInterval(cloneChildrens, animationTime)
+
+const setMarqueeListsWidth = () => {
+    const items = marqueeLists[0].querySelectorAll('.marquee__item')
+
+    let totalElementsWidth = 0
+    for(let i = 0; i < 3; i++) {
+        totalElementsWidth += items[i].offsetWidth + gap
+    }
+
+    Array.from(marqueeLists).forEach((marqueeList) => {
+        marqueeList.style.width = totalElementsWidth + 'px'
+    })
+}
+setMarqueeListsWidth()
+window.addEventListener('resize', setMarqueeListsWidth, true)
+/* < marquees */
